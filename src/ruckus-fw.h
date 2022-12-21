b89acd7d5a9adf020bff93e72b643e91d8da0903
ms264556 <github.ssh.ms264556@recursor.net>
2022-12-21 07:33:54 +0000

    typo

--- src/ruckus-fw.h
+++ src/ruckus-fw.h
@@ -11,12 +11,12 @@ static const char HDR_MAGIC[] = "RCKS";
 
 struct bin_hdr
 {
-	// This is structure is store in network byte order.
+	// This struct is stored in network byte order.
 	
-	uint8_t magic[4];		 // 0x0
+	uint8_t magic[4];	// 0x0
 	uint32_t next_image;	 // 0x4 // offset to next image ( reserved )
-	uint8_t invalid;		 // 0x8 // only autoboot from this image if invalid is 0
-	uint8_t hdr_len;		 // 0x9 length of this header
+	uint8_t invalid;	 // 0x8 // only autoboot from this image if invalid is 0
+	uint8_t hdr_len;	 // 0x9 length of this header
 	uint8_t compression[2]; // 0xa // compression scheme, default l7, [l7|zl|xx]
 
 	uint32_t entry_point; // 0xc // Execution entry point
@@ -32,9 +32,9 @@ struct bin_hdr
 	// the following fields are for validating that we have the correct image
 	// The rule is that a value of 0 means don't care
 	//
-	uint8_t product;	   // 0x3c // product class, e.g. AP vs Adapter
+	uint8_t product;	// 0x3c // product class, e.g. AP vs Adapter
 	uint8_t architecture; // 0x3d // mips, le/be, etc.
-	uint8_t chipset;	   // 0x3e // e.g AR531X
+	uint8_t chipset;	// 0x3e // e.g AR531X
 	uint8_t board_type;   // 0x3f // V54 board type
 
 	uint8_t customer[32];	 // 0x40 // customer ID, NULL ==> any customer
@@ -45,7 +45,7 @@ struct bin_hdr
 	uint32_t load_address; // 0x80
 	uint32_t image_type;	// 0x84 // 0=Unsigned Image (UI) / 1=Intermediate Signed Image (ISI) / 2=Fully Signed Image (FSI)
 	uint32_t tail_offset;	// 0x88 // Offset to the signatures within the binl7. Only ISI stores signatures within the binl7, so this is 0 for UI & FSI images.
-	uint8_t _pad_end[20];	// pad out to 128 bytes
+	uint8_t _pad_end[20];	// pad out to 160 bytes
 };
 
 void ntoh_hdr(struct bin_hdr *binhdr);
