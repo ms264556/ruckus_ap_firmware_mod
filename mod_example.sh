8c8ade07cd119d0c477b6d1b6ae12fa4e6595313
ms264556 <ms264556>
2023-01-16 02:15:43 +0000

    Updated mod_example.sh - tested successfully  on ZF7962

--- mod_example.sh
+++ mod_example.sh
@@ -1,78 +1,44 @@
 #!/bin/bash
 
-# This sample implements the repeater/extender changes suggested by aiman_al_hadhr1 here:-
+# This sample is inspired by the repeater/extender changes suggested by aiman_al_hadhr1 here:-
 # https://community.ruckuswireless.com/t5/Access-Points-Indoor-and-Outdoor/Ruckus-7341-U-act-as-repeater/m-p/31821/highlight/true#M8936
+# (I couldn't make the suggested changes work, so I instead borrowed the P300 bridging config).
 
-function WlansWlan30 {
-    pushd $1
+function WlansBridge {
+    pushd $1 >/dev/null
     sudo chmod o=rw *
-    echo -n up >wlan-state
-    echo -n ap >wlan-type
-    echo -n wlan-trunk-ap >wlan-userdef-text
-    echo -n 0 >wlan-ssid-hide
-    echo -n wlan-trunk-ap >wlan-ssid
-    echo -n 30 >wlan-mac-alloc-unit
-    echo -n 0 >wlan-if-flags
-    sudo chmod o=r *
-    popd
-}
+    sudo chmod o=rwx .
 
-function WlansWlan31 {
-    pushd $1
-    sudo chmod o=rw *
-    echo -n up >wlan-state
-    echo -n sta >wlan-type
-    echo -n wlan-trunk-station >wlan-userdef-text
-    echo -n wlan-trunk-station >wlan-ssid
-    echo -n 31 >wlan-mac-alloc-unit
+    echo -n 0 >wlan-auth-type
+    echo 2 >wlan-cipher-type
+    echo 1 >wlan-created-defined
+    echo 0 >wlan-encrypt-state
+    echo -n 1 >wlan-encrypt-type
     echo -n 1 >wlan-if-flags
-    sudo chmod o=r *
-    popd
-}
-
-function IfsWlan30 {
-    pushd $1
-    sudo chmod o=rw *
-    sudo chmod o=rwx .
-    echo -n 0 >br
-    echo -n 1 >port-type
-    echo -n 1 >untag-vid
-    echo -n 1 >vlans
-    rm -f port-isolation-mode
+    echo -n 1 >wlan-if-parent
+    echo -n 1 >wlan-init-noup
+    echo allow >wlan-isallowed
+    echo uplink_ssid >wlan-ssid
+    echo -n up >wlan-state
+    echo sta >wlan-type
+    echo Wireless Bridge >wlan-userdef-text
+    rm -f wlan-wpa-eap-enable
+    echo -n uplink_pass >wlan-wpa-passphrase
+    echo -n 2 >wlan-wpa-type
+    
     sudo chmod o=rx .
     sudo chmod o=r *
-    popd
-}
-
-function IfsWlan31 {
-    pushd $1
-    sudo chmod o=rw *
-    sudo chmod o=rwx .
-    echo -n 1 >port-type
-    echo -n 1 >untag-vid
-    echo -n 1-4094 >vlans
-    rm -f port-isolation-mode
-    sudo chmod o=rx .
-    sudo chmod o=r *
-    popd
-}
-
-function IncreaseMaxWlans {
-    sudo find . -name "wlan-maxwlans2" -exec chmod o=rw {} \; -exec sh -c 'echo -n 20 > {}' \;  -exec chmod o=rw {} \;
+    popd >/dev/null
 }
 
 function ModSoloImageAddUplinks {
-    pushd packaging/rootfs
-    WlansWlan30 "defaults/common/4bss/wlans/wlan30"
-    WlansWlan30 "defaults/common/hz-4bss/wlans/wlan30"
-    WlansWlan31 "defaults/common/4bss/wlans/wlan31"
-    WlansWlan31 "defaults/common/hz-4bss/wlans/wlan31"
-    IfsWlan30 "defaults/common/4bss/ifs/wlan30"
-    IfsWlan30 "defaults/common/hz-4bss/ifs/wlan30"
-    IfsWlan31 "defaults/common/4bss/ifs/wlan31"
-    IfsWlan31 "defaults/common/hz-4bss/ifs/wlan31"
-    IncreaseMaxWlans
+    pushd packaging/rootfs/defaults
+    
+    while IFS= read -d '' wlandir; do
+        WlansBridge "${wlandir}" </dev/null
+    done < <(find . -type d -path "*/$1" -print0)
+    
     popd
 }
 
-ModSoloImageAddUplinks >/dev/null
+ModSoloImageAddUplinks "wlans/wlan15"
